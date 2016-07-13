59416d22719ecdb847d00546ea6746377ad193d0
Stella Ma <sm873x@gmail.com>
2016-07-13 14:45:15 -0400

    JS - Added login ajax to retrive token. HTML - added article class to message section in order to hold text for errors, but also to be used for message entries

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -2,5 +2,49 @@
     'use strict';
     window.chat = ns = ( ns || {} );
 
+    //TODO - messageHandler function here
+    window.chat.listenForMessages(function messageHandler(data) {
+        console.log(data);
+    });
 
+
+    //TODO - post Ajax request to /login to send username
+    $('.login').on( 'submit', function login(e) {
+        e.preventDefault();
+
+        var username = $('.username').val();
+        loginUsername(username);
+    });
+
+    function loginUsername(username) {
+        $.ajax({
+            url: '/login',
+            method: 'post',
+            headers: {'Content-Type':'application/json'},
+            data: JSON.stringify({ 'username': username }),
+            dataType: 'json'
+        })
+        .done(function loginSuccess(data) {
+            // getToken(data);
+            console.log(data);
+        })
+        .fail(function loginFail(xhr) {
+            var $msgTxt = $('.msgTxt').val();
+            handleLoginError(xhr, $msgTxt);
+        });
+    }
+
+    // function getToken(data) {
+    //     data.
+    // }
+
+    function handleLoginError(xhr, elem) {
+        if (xhr.status === 404) {
+            $(elem).text('Ruh roh, what did you do?');
+        } else {
+            $(elem).text('Hmm...not sure what\'s happening here');
+        }
+    }
+
+    //TODO - submits chat form with post Ajax request
 })(window.chat);
